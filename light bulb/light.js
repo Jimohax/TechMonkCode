97d6363e7f94847d50bf56c86976009d2fb3d6a7
Jamie Ohax <jimohax@gmail.com>
2024-02-21 15:41:36 +0100

    lightbulb flip added

--- light bulb/light.js
+++ light bulb/light.js
@@ -1,10 +1,22 @@
+let bulb = document.getElementById("bulbOff");
 
-let bulb = document.getElementById('bulbOff');
-
-function turnOn(){
-    bulb.src = 'LightBulbOn.jpg'
+function turnOn() {
+	bulb.src = "LightBulbOn.jpg";
+}
+function turnOff() {
+	bulb.src = "LightBulbOff.jpg";
 }
 
-function turnOff(){
-    bulb.src = 'LightBulbOff.jpg'
+function lightBulb(flip) {
+    
+    if (flip > 0){
+        console.log( "Light has been turned on "  + flip);
+
+        bulb.src = "LightBulbOn.jpg"
+    }else{
+        console.log( "Light has been turned off "  + flip);
+
+        bulb.src = "LightBulbOff.jpg"
+    }
+    console.log("We did it ");
 }
