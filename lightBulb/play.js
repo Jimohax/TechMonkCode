a7dfc7ca762c37bc0fc91d93ef0989ec7ef5864a
Jamie Ohax <jimohax@gmail.com>
2024-02-28 15:12:10 +0100

    counter

--- lightBulb/play.js
+++ lightBulb/play.js
@@ -1,12 +1,31 @@
 let a;
 let b;
 let c;
+let counter = 0 ;
+
+document.getElementById("increase").onclick = function(){
+    counter += 1
+    document.getElementById('counter').innerHTML = counter 
+}
+document.getElementById("decrease").onclick = function(){
+    counter -= 1
+    document.getElementById('counter').innerHTML = counter 
+}
+document.getElementById("reset").onclick = function(){
+    counter = 0
+    document.getElementById('counter').innerHTML = counter 
+}
+
 
 
 function calc() {
-    a = window.prompt("What is side A?") ;
-    b = window.prompt("What is side B?") ;
-    c = (a*2 + b*2)
-    console.log("The answer is " + c);
+    a = Number(document.getElementById("sideA").value) ;
+    b = Number(document.getElementById("sideB").value) ;
     
+    c = (a*2 + b*2)
+    // document.getElementById("result").innerHTML = c
+    
+    // console.log("The answer is " + c);
+
+    document.getElementById("result").innerHTML = "The answer is" + " " + c
 }
